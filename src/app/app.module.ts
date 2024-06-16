3384c4b8599046d9487f9acd962a197ef15c3094
elmehdi-123 <66562903+elmehdi-123@users.noreply.github.com>
2024-06-16 20:10:49 +0200

    files organized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,32 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
-import {HotelListComponent} from "./hotel-list/hotel-list.component";
+
+
 import {FormsModule} from "@angular/forms";
 import {registerLocaleData} from "@angular/common";
 import localeFr from "@angular/common/locales/fr"
-import {ReplaceComma} from "./shared/pipes/replace-comma.pipe";
-import {StarRatingComponent} from "./shared/components/star-rating/star-rating.component";
 import {HttpClientModule} from "@angular/common/http";
 import {HomeComponent} from "./home/home.component";
-
-import {RouterModule} from "@angular/router";
-import {HotelListService} from "./hotel-list/hotel-list.service";
-import {HotelDetailComponent} from "./hotel-list/hotel-detail/hotel-detail.component";
-import {HotelDetailsGuard} from "./hotel-list/hotel-detail.guard";
+import { HotelModule } from './hotels/hotel.module';
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 registerLocaleData(localeFr , 'fr');
 @NgModule({
   declarations: [
     AppComponent,
-    HotelListComponent,
-    ReplaceComma,
-    StarRatingComponent,
-    HomeComponent,
-    HotelDetailComponent
+    HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-      HttpClientModule,
-      RouterModule.forRoot([
-        {path: 'home' , component: HomeComponent },
-        {path: '' , redirectTo: 'home' , pathMatch: 'full' },
-        {path: 'hotels/:id' , component: HotelDetailComponent,
-        canActivate: [HotelDetailsGuard]},
-        {path: 'hotels' , component: HotelListComponent },
-        {path: '**' , redirectTo: 'home' , pathMatch: 'full' }
-      ])
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HotelModule,
+    AppRoutingModule,
+    RouterLink,
+    RouterOutlet
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
